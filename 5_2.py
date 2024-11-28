632ef8f2cc72af36fe1b3622594894347e3d71e2
好想出去玩 <aniplyhe@outlook.com>
2024-11-28 22:32:07 +1300

    Update 5_2.py

--- 5_2.py
+++ 5_2.py
@@ -1,5 +1,8 @@
 # Once “done” is entered, print out the total, count, and average of the integers. 
-# If the user enters anything other than a integers, detect their mistake using try and except and print an error message and skip to the next integers.
+# If the user enters anything other than a integers, detect their mistake using try and except 
+# and print an error message and skip to the next integers.
+# Write another program that prompts for a list of numbers as above 
+# and at the end prints out both the maximum and minimum of the numbers instead of the average.
 
 total = 0
 count = 0
